c4b214079b27db8cbf0ed0f86cc5cb70b56542d3
Jonathan Chen <jonathan.chen@wustl.edu>
2017-12-03 17:56:18 -0600

    movement

---
+++ Hallow/Character.swift
@@ -0,0 +1,95 @@
+//
+//  Player.swift
+//  Hallow
+//
+//  Created by Jonathan Chen on 12/3/17.
+//  Copyright © 2017 Joyful Bad Juniper. All rights reserved.
+//
+
+import SpriteKit
+
+enum Direction {
+    case left, right
+}
+
+class Character: SKSpriteNode {
+    
+    let prefix: String
+    var orientation: Direction = .right
+    var jumping = false
+    
+    init(_ prefix: String) {
+        self.prefix = prefix
+        
+        let texture = SKTexture(imageNamed: "\(prefix)_stand")
+       
+        super.init(texture: texture, color: .gray, size: texture.size())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        self.prefix = "player"
+        super.init(coder: aDecoder)
+    }
+    
+    func stand() {
+        self.texture = SKTexture(imageNamed: "\(prefix)_stand")
+        removeAction(forKey: "move")
+    }
+    
+    func jump() {
+        if (!jumping) {
+            print("jumping")
+            
+            texture = SKTexture(imageNamed: "\(prefix)_jump")
+            
+            let moveUp = SKAction.moveBy(x: 0, y: 75, duration: 0.25)
+            let movedown = SKAction.moveBy(x: 0, y: -75, duration: 0.25)
+            
+            jumping = true
+            
+            run(SKAction.sequence([moveUp, movedown])) {
+                self.jumping = false
+            }
+        }
+    }
+    
+    func walk(_ direction: Direction) {
+        
+        print("walking \(direction == .left ? "left" : "right")")
+        
+        let walkingFrames = [
+            SKTexture(imageNamed: "\(prefix)_walk1"),
+            SKTexture(imageNamed: "\(prefix)_walk2")
+        ]
+
+        let walkAnimation = SKAction.repeat(SKAction.animate(with: walkingFrames, timePerFrame: 1/4), count: 2)
+        
+        let moveLeft = SKAction.moveBy(x: -200, y: 0, duration: walkAnimation.duration)
+        let moveRight = SKAction.moveBy(x: 200, y: 0, duration: walkAnimation.duration)
+
+        let walkAndMoveRight = SKAction.group([walkAnimation, moveRight])
+        let walkAndMoveLeft = SKAction.group([walkAnimation, moveLeft])
+        
+        face(direction)
+        
+        if (orientation == .left) {
+            run(SKAction.repeatForever(walkAndMoveLeft), withKey: "move")
+        } else if (orientation == .right) {
+            run(SKAction.repeatForever(walkAndMoveRight), withKey: "move")
+        }
+    }
+
+    func face(_ direction: Direction) {
+        guard orientation != direction else { return }
+        
+        orientation = direction
+        
+        let faceLeft = SKAction.scaleX(to: -1, y:1, duration:0.0)
+        let faceRight = SKAction.scaleX(to: 1,  y:1, duration:0.0)
+        
+        switch (direction) {
+        case .left: run(faceLeft)
+        case .right: run(faceRight)
+        }
+    }
+}
--- Hallow/GameViewController.swift
+++ Hallow/GameViewController.swift
@@ -11,6 +11,11 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+    
+    var player = Character("player")
+    var moving = false
+
+    var taps: [(UITouch, Direction)] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,8 +26,11 @@ class GameViewController: UIViewController {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
+                scene.addChild(self.player)
+                
                 // Present the scene
                 view.presentScene(scene)
+                
             }
             
             view.ignoresSiblingOrder = true
@@ -30,38 +38,44 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
             view.isMultipleTouchEnabled = true
-        }
-    }
-    
-    func move(x: CGFloat) {
-        if let scene = SKScene(fileNamed: "Clocktower") {
-            print("moving \(x < 0 ? "left" : "right")")
-            
-            let ground = scene.childNode(withName: "CT Ground")
-            ground?.position.x -= x
-            
-            let bg = scene.childNode(withName: "CT Background")
-            bg?.position.x -= x * 0.5
             
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if (touches.count > 1) { // jump
-            print("jump")
-        } else { // move
-            guard let scene = SKScene(fileNamed: "Clocktower") else { return }
-            guard let touch = touches.first?.location(in: scene) else { return }
+        
+        guard let scene = SKScene(fileNamed: "Clocktower") else { return }
+        
+        if let touch = touches.first {
             
-            let x = touch.x / scene.size.width
+            let position = touch.location(in: scene).x / view.frame.size.width
+            let orientation = position < 0.5 ? Direction.left : Direction.right
             
-            if (x < 0.5) {
-                move(x: -5)
-            } else {
-                move(x: 5)
-            }
-
+            let tap = (touch, orientation)
+            taps.append(tap)
+            
+            player.walk(taps[0].1)
+            
         }
+        
+        if taps.count > 1 {
+            player.jump()
+        }
+
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+            taps = taps.filter { return $0.0 != touch }
+        }
+        
+        if taps.count > 0 {
+            player.walk(taps[0].1)
+        } else {
+            player.stand()
+        }
+        
     }
 
     override var shouldAutorotate: Bool {
